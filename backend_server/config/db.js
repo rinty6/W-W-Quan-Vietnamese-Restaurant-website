9dd37badb098b65c65b327fa7fac423d39cb39e1
unknown <duongphuthinh2001@gmail.com>
2025-09-04 23:07:28 +0930

    planetscale database

--- backend_server/config/db.js
+++ backend_server/config/db.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const pool = mysql.createPool(
+  process.env.DATABASE_URL
+    ? { uri: process.env.DATABASE_URL }
+    : {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 10,
+      }
+);
 
 module.exports = pool;
